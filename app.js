7f246e29208ead61eca629dc73eb1bf3d63030d5
sakthi leela <sakthi.leela@oracle.com>
2021-01-13 19:42:26 +0530

    commit assignment2

--- app.js
+++ app.js
@@ -1,77 +1,34 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingListApp', [])
-.controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController)
-.provider('ShoppingListService', ShoppingListServiceProvider)
+angular.module('LunchCheckerApp', [])
+.controller('LunchCheckerController', LunchCheckerController);
 
-
-ToBuyController.$inject = ['ShoppingListService'];
-function ToBuyController(ShoppingListService) {
-  var tobuy = this;
-
-  tobuy.toBuyItems = ShoppingListService.getItemsToBuy();
-
-  tobuy.buyItem = function (itemIndex) {
-      ShoppingListService.buyItemFromList(itemIndex);
-      ShoppingListService.removeItemFromBuyList(itemIndex);
-  };
-}
-
-AlreadyBoughtController.$inject = ['ShoppingListService'];
-function AlreadyBoughtController(ShoppingListService) {
-  var alreadybought = this;
-
-  alreadybought.alreadyboughtitems = ShoppingListService.getItemsAlreadyBought();
-}
-
-
-
-function ShoppingListService() {
-  var service = this;
-
-  // List of shopping items
-  var toBuyItems = [
-    { name: "cookies", quantity: 10 },
-    { name: "chips", quantity: 5 },
-    { name: "cola", quantity: 3 },
-    { name: "steaks", quantity: 2 },
-    { name: "beers", quantity: 20 },
-  ];
-
-  var alreadyBoughtItems = [];
-
-  service.buyItemFromList = function (itemIndex) {
-      var item = {
-        name: toBuyItems[itemIndex].name,
-        quantity: toBuyItems[itemIndex].quantity
-      };
-      alreadyBoughtItems.push(item);
+LunchCheckerController.$inject = ['$scope'];
+function LunchCheckerController($scope) {
+  $scope.countNoOfItems = function () {
+  var count = 0;
+    if ($scope.items) {
+      var array = $scope.items.split(',');
+      for (var i in array) {
+        if (array[i].trim().length != 0) {
+          count++;
+        }
+      }
+    }
+    $scope.message = displayOutputMsg(count);
   };
 
-  service.removeItemFromBuyList = function (itemIndex) {
-    toBuyItems.splice(itemIndex, 1);
-  };
-
-  service.getItemsToBuy = function () {
-    return toBuyItems;
-  };
-
-  service.getItemsAlreadyBought = function () {
-    return alreadyBoughtItems;
-  };
-}
-
-
-function ShoppingListServiceProvider() {
-  var provider = this;
-
-  provider.$get = function () {
-    var shoppingList = new ShoppingListService();
-
-    return shoppingList;
-  };
+  function displayOutputMsg(count) {
+    if (count == 0) {
+      return 'Please enter dishes';
+    }
+    else if (count <= 3) {
+      return 'Enjoy!!';
+    } else {
+      return 'Too much!!';
+    }
+  }
 }
 
 })();
